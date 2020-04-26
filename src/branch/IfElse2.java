2669fc1eae02c92fa9e7e23c0c6f838a94cb0306
mikahikaai <mikahikaai@gmail.com>
2020-04-26 20:24:28 +0800

    Hidup itu sulit, kawan :(

---
+++ src/branch/IfElse2.java
@@ -0,0 +1,14 @@
+package branch;
+
+public class IfElse2 {
+    public static void main(String[] args) {
+        double inginMenikahUmur = 24;
+        double umurSaatIni = 27;
+
+        if (umurSaatIni < inginMenikahUmur){
+            System.out.println("Berhayal terooooossss!"); //fakta
+        }else{
+            System.out.println("kehidupan itu sulit, kawan :("); //bukan hoax
+        }
+    }
+}
