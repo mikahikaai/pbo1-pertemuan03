0a67955d9a5dba4d7230d25e5c652cc595f6c0a1
mikahikaai <mikahikaai@gmail.com>
2020-04-26 20:15:41 +0800

    Gambaran Hukum Rimba Rumah Tangga

---
+++ src/branch/IfElse.java
@@ -0,0 +1,14 @@
+package branch;
+
+public class IfElse {
+    public static void main(String[] args) {
+        double salaryAyah = 2500000;
+        double salaryIbu = 3500000;
+
+        if (salaryAyah < salaryIbu){
+            System.out.println("Ibuku selalu memarah-marahi ayah"); //fakta
+        }else{
+            System.out.println("Ayahku selalu dimarah-marahi ibu "); //bukan hoax
+        }
+    }
+}
