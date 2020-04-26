a5b6bcfab7c3967385803f9995507ecf60f349d0
mikahikaai <mikahikaai@gmail.com>
2020-04-26 20:56:20 +0800

    jakanya kdd parang. . bini+=1

---
+++ src/branch/Switch.java
@@ -0,0 +1,21 @@
+package branch;
+
+public class Switch {
+    public static void main(String[] args) {
+        int bini = 4;
+        switch (bini){
+            case 1:
+                System.out.println("Alhamdulillah satu aja gen dlu, satu aja kada habis");
+                break;
+            case 2:
+                System.out.println("Alhamdulillah mun tulak jauh bini sudah ada kawannya");
+                break;
+            case 3:
+                System.out.println("Alhamdulillah mun bekelahi ada nang melerai");
+            case 4:
+                System.out.println("Alhamdulillah kesampaian jua meumpati sunah rasul");
+            default:
+                System.out.println("Handak nambah pulang, bini membawa parang");
+        }
+    }
+}
