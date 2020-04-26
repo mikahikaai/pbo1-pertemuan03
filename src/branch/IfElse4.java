79c14846ca423cf3f5807311715adda4283fb08a
mikahikaai <mikahikaai@gmail.com>
2020-04-26 20:37:30 +0800

    rencana masa depan (terencana)

---
+++ src/branch/IfElse4.java
@@ -0,0 +1,18 @@
+package branch;
+
+public class IfElse4 {
+    public static void main(String[] args) {
+        double gajiSaatIni = 2500000;
+        double inginMenikahPasSudahGajiBanyak = 3500000;
+
+        if (gajiSaatIni < inginMenikahPasSudahGajiBanyak){
+            System.out.println("Ayo bro kerja terus supaya bisa nikah"); //rencana awal
+        }else if (gajiSaatIni>inginMenikahPasSudahGajiBanyak){
+            double tabungan = inginMenikahPasSudahGajiBanyak-gajiSaatIni;
+            System.out.println("Alhamdulillah sudah siap nikah, sisa tabungan:", tabungan); //bukan hoax
+        }else{
+            System.out.println("Loh malah corona, batal nikah dong syalalalala~~~~"); //saat ini
+        }
+
+    }
+}
