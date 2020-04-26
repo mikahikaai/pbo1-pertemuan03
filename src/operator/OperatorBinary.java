f31bea85bf75d95ac13473bdec6822f5f5bb01e3
mikahikaai <mikahikaai@gmail.com>
2020-04-26 19:59:06 +0800

    Arithmetic Binary Operator

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,18 @@
+package operator;
+
+public class OperatorBinary {
+
+    String nama = "Rian Septiadi";
+    int jumlahSks = 24;
+    double indeksPrestasiKomulatif = 4.00;
+
+    public static void main(String[] args) {
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+    }
+
+}
