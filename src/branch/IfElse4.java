891e065086535c2cc464d3fbe726c0c73cab3a41
mikahikaai <mikahikaai@gmail.com>
2020-04-26 20:43:31 +0800

    batal nikah gara2 corona

--- src/branch/IfElse4.java
+++ src/branch/IfElse4.java
@@ -9,7 +9,7 @@ public class IfElse4 {
             System.out.println("Ayo bro kerja terus supaya bisa nikah"); //rencana awal
         }else if (gajiSaatIni>inginMenikahPasSudahGajiBanyak){
             double tabungan = inginMenikahPasSudahGajiBanyak-gajiSaatIni;
-            System.out.println("Alhamdulillah sudah siap nikah, sisa tabungan:", tabungan); //bukan hoax
+            System.out.println("Alhamdulillah sudah siap nikah, sisa tabungan: "+ tabungan); //bukan hoax
         }else{
             System.out.println("Loh malah corona, batal nikah dong syalalalala~~~~"); //saat ini
         }
